95847235bd97c74bd1dafa19556a05b70f373ec3
Holger Schmermbeck <hosch@mailbox.org>
2023-02-01 08:51:01 +0100

    use CACHE_ALL_POLICIES in .env (#4)
    use CACHE_ALL_POLICIES in .env file to enable / disable global policy caching

--- config/policy-soft-cache.php
+++ config/policy-soft-cache.php
@@ -4,5 +4,5 @@ return [
     /*
      * When enabled, the package will cache the results of all Policies in your Laravel application
      */
-    'cache_all_policies' => true,
+    'cache_all_policies' => env('CACHE_ALL_POLICIES', true),
 ];
